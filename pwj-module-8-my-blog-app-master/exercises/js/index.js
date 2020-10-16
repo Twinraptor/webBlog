3ac4074bac9f453d1c6449d0c02b7690ee522d34
twinraptor <mythosx@hotmail.com>
2020-10-15 18:56:17 -0700

    all blog pulled and populated on index page

--- pwj-module-8-my-blog-app-master/exercises/js/index.js
+++ pwj-module-8-my-blog-app-master/exercises/js/index.js
@@ -8,8 +8,33 @@ window.onload = () => {
 }
 
 const getPosts = () => {
+    fetch(API_URL, {
+        method:'GET'
+
+    }).then((response)=> {
+        return response.json();
+    }).then((data) => {
+        buildPosts(data);
+    })
 }
 
 const buildPosts = (blogPosts) => {
-
+    let blogPostsContent = '';
+    console.log(blogPosts)
+    for(blogPost of blogPosts) {
+        postDate = new Date(parseInt(blogPost.added_date)).toDateString();
+        const postImage= `${API_BASE_URL}${blogPost.post_image}`
+        blogPostsContent +=
+        `<div class="blogpost">
+            <div class="blogpost-image" style="background-image: url('${postImage}')"></div>
+            <div class="blogpost-content">
+                <div class="blogpost-date">${postDate}</div>
+                <div class="blogpost-title">${blogPost.title}</div>
+                <div class="blogpost-preview">
+                    ${blogPost.content}
+                </div>
+            </div>
+        </div>`
+    }
+    document.querySelector('.blog-Posts').innerHTML = blogPostsContent;
 }
